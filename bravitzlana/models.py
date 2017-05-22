2e190962a39c8cce77531c53dd704c33102c4da2
Marek Kapolka <marek.kapolka@gmail.com>
2017-05-22 09:26:10 -0700

    Dang look at all this crazy crap

--- bravitzlana/models.py
+++ bravitzlana/models.py
@@ -3,11 +3,17 @@ import uuid
 from django.db import models
 
 
+def generate_save_path(game, filename):
+    return 'games/%s/%s' % (game.uuid, filename)
+
+
 class Game(models.Model):
+    session_key = models.CharField(max_length=128, null=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
-    description = models.TextField()
-    save_data = models.FileField(upload_to='games/')
+    name = models.CharField(max_length=128, default='')
+    description = models.TextField(default='')
+    save_data = models.FileField(upload_to=generate_save_path)
+    is_new_template = models.BooleanField(default=False)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
--- bravitzlana/views.py
+++ bravitzlana/views.py
@@ -1,5 +1,9 @@
+import uuid
+
 from django.db import transaction
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from bravitzlana.models import Game
@@ -9,13 +13,16 @@ from bravitzlana.models import Game
 def upload(request):
     if request.method != 'POST':
         return HttpResponseBadRequest("Malformed upload.")
-    with transaction.atomic():
-        Game.objects.create(
-            name=request.POST['name'],
-            description=request.POST['description'],
-            save_data=request.FILES['save']
-        )
-        return HttpResponse("Looks dope dude")
+    try:
+        with transaction.atomic():
+            game = Game.objects.create(
+                name=request.POST['name'],
+                description=request.POST['description'],
+                save_data=request.FILES['save']
+            )
+            return HttpResponse(request.build_absolute_uri(reverse('play', args=[game.uuid])))
+    except Exception as e:
+        return HttpResponseServerError(str(e))
 
 
 @csrf_exempt
@@ -25,3 +32,34 @@ def save_data(request, uuid):
         return HttpResponse(game.save_data, content_type='text/plain')
     except Game.DoesNotExist:
         return HttpResponseNotFound
+
+
+def play(request, uuid, uuid_override=None):
+    try:
+        game = Game.objects.get(uuid=uuid)
+        context = {
+            'uuid': game.uuid,
+            'name': game.name,
+            'description': game.description
+        }
+        if uuid_override:
+            context['uuid_override'] = uuid_override
+        return render(request, 'player/index.html', context)
+    except Game.DoesNotExist:
+        return HttpResponseNotFound
+
+
+def new(request, uuid_override):
+    print(uuid_override)
+    if not uuid_override:
+        return redirect('new', uuid.uuid4())
+    else:
+        return play(request, Game.objects.filter(is_new_template=True)[0].uuid, uuid_override)
+
+
+def games(request):
+    context = {
+        'games': Game.objects.order_by('-created')[:10]
+    }
+    print(context)
+    return render(request, 'games.html', context)
